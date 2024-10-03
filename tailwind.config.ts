e46119fffa090c48652bc24ef4c99914b3f44d6c
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-10-02 19:41:20 -0600

    feat: add tailwindcss primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,22 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        primary: {
+          50: '#E6D9FF',
+          100: '#CAB0FF',
+          200: '#AF86FF',
+          300: '#935DFF',
+          400: '#7733FF',
+          500: '#662CDA',
+          600: '#5524B6',
+          700: '#441D91',
+          800: '#33166D',
+          900: '#220E48',
+        },
       },
     },
   },
