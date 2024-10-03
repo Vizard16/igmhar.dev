63b2262640fae719e0cfcb9c25ae0eaef1bdbd4d
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-10-02 19:42:23 -0600

    refactor: update  description in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,19 @@ import React from 'react';
 
 const Index: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Igmhar Sánchez | Software Developer</title>
-        <meta name="description" content="Description" />
+        <meta name="description" content="Hi!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Igmhar Sánchez</h1>
-      </main>
-    </div>
+      <>
+        <p>Hello</p>
+        <h1 className="text-4xl font-extrabold">
+          Igmhar Sánchez, Software Developer & Mechatronics Engineer
+        </h1>
+      </>
+    </>
   );
 };
 
