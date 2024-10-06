ac1f94c5c118097e5e51755a2f8c95004b32b37e
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-10-06 14:38:41 -0600

    Refactor package dependencies and add postcss-nesting plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,32 @@ import type { PluginAPI } from 'tailwindcss/types/config'; // Import PluginAPI t
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/misc/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  mode: 'jit',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    animationDelay: {
+      100: '100ms',
+      200: '200ms',
+      300: '300ms',
+      400: '400ms',
+      500: '500ms',
+      600: '600ms',
+      700: '700ms',
+      800: '800ms',
+      900: '900ms',
+    },
+    animationDuration: {
+      100: '100ms',
+      200: '200ms',
+      300: '300ms',
+      400: '400ms',
+      500: '500ms',
+      600: '600ms',
+      700: '700ms',
+      800: '800ms',
+      900: '900ms',
+    },
     extend: {
       colors: {
         primary: {
@@ -26,8 +44,31 @@ module.exports = {
           800: '#33166D',
           900: '#220E48',
         },
-        accent: {
-          700: '#FF5733', // Custom accent color
+      },
+      animation: {
+        'fade-in': 'fade-in 400ms ease-out forwards',
+        'fade-left': 'fade-left 400ms ease-out forwards',
+      },
+      keyframes: {
+        'fade-in': {
+          from: {
+            transform: 'translate3d(0,-2rem,0)',
+            opacity: 0,
+          },
+          to: {
+            transform: 'translate3d(0,0,0)',
+            opacity: 1,
+          },
+        },
+        'fade-left': {
+          from: {
+            transform: 'translate3d(-1.5rem,0,0)',
+            opacity: 0,
+          },
+          to: {
+            transform: 'translate3d(0,0,0)',
+            opacity: 1,
+          },
         },
       },
     },
@@ -43,6 +84,31 @@ module.exports = {
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      );
+      matchUtilities(
+        {
+          'animation-delay': (value) => ({
+            animationDelay: value,
+          }),
+        },
+        { values: theme('animationDelay') }
+      );
+      matchUtilities(
+        {
+          'animation-duration': (value) => ({
+            animationDuration: value,
+          }),
+        },
+        { values: theme('animationDuration') }
+      );
+
+      matchUtilities(
+        {
+          highlight: (value) => ({
+            boxShadow: `inset 0 1px 0 0 ${value}`,
           }),
         },
         { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
