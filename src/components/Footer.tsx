787c951ec7e00576f1af07c0435938869ec33277
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-10-06 01:32:23 -0600

    feat: Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="border-t border-t-slate-100 py-8 px-4 text-sm text-gray-900">
+      <section className="mb-12 font-semibold">
+        <ul className="flex flex-wrap justify-center gap-x-4 gap-y-1">
+          <li>
+            <Link href="/">Mexico</Link>
+          </li>
+          <li className="w-full sm:hidden"></li>
+          <li>
+            <Link href="/">Source Code</Link>
+          </li>
+          <li>
+            <Link href="/">Algo</Link>
+          </li>
+          <li>
+            <Link href="/">Mail</Link>
+          </li>
+        </ul>
+      </section>
+      <section className="mb-2">
+        <ul className="flex justify-center gap-1 text-xs">
+          <li>
+            <Link href="/" className="hover:underline">
+              Holaa
+            </Link>
+          </li>
+          <li>·</li>
+          <li>
+            <Link href="/" className="hover:underline">
+              Pudiplay
+            </Link>
+          </li>
+        </ul>
+      </section>
+      <small className="flex justify-center font-semibold">
+        &copy; 2024, Igmhar Sánchez
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
