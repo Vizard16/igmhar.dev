4bf9d3f41d1691663af3cc18228504b8033ea77f
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-10-07 14:56:00 -0600

    Refactor code to extract FeaturedCard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import type { NextPage } from 'next';
 import TechSlider from '@/components/TechSlider';
 import Image from 'next/image';
 import Quote from '@/components/Quote';
+import FeaturedCard from '@/components/FeaturedCard';
 
 import {
   SparklesIcon,
@@ -13,38 +14,11 @@ import {
   DocumentIcon,
 } from '@/components/Icons';
 
-import type { ReactElement } from 'react';
-
-interface FeaturedCardProps {
-  icon: ReactElement;
-  title: string;
-  desc: string;
-}
-
-const FeaturedCard = ({ icon, title, desc }: FeaturedCardProps) => {
-  return (
-    <div className="relative z-10 flex-1 rounded-2xl border border-slate-100 bg-white dark:border-slate-800 dark:bg-slate-900">
-      <div className="absolute inset-x-0 inset-y-10 z-[-1] border-t border-slate-100 dark:border-slate-800"></div>
-      <div className="absolute inset-y-0 inset-x-10 z-[-1] border-l border-slate-100 dark:border-slate-800"></div>
-      {/* title */}
-      <div className="mt-5 mr-4 ml-6 flex items-center gap-6 rounded-full bg-slate-100 dark:bg-slate-800">
-        <div className="-m-2">{icon}</div>
-        <div className="truncate py-2 pr-4 font-bold text-gray-700 dark:text-slate-300">
-          {title}
-        </div>
-      </div>
-      <div className="pb-6 pl-16 pt-4 pr-6 text-sm text-gray-500 dark:text-slate-400">
-        {desc}
-      </div>
-    </div>
-  );
-};
-
 const Index: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Igmhar • Software Developer</title>
+        <title>Igmhar &middot; Software Developer</title>
         <meta name="description" content="Hi!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -61,20 +35,25 @@ const Index: NextPage = () => {
         <div className="content-wrapper">
           <div className="relative">
             {/* title */}
-            <h1 className="text-gray-600 dark:text-slate-400">
-              <span className="mb-3 block animate-fade-left text-5xl font-bold md:mb-4 md:text-7xl">
-                Hello! I am{' '}
-                <strong className="font-extrabold text-gray-900 dark:text-slate-50">
-                  Igmhar
-                </strong>
-                ,
-              </span>
-              <span className="block animate-fade-left text-xl animation-delay-100 md:text-2xl">
-                <strong className="font-bold"> Jr. Software</strong>{' '}
-                <strong className="font-bold">Developer</strong> and{' '}
-                <strong className="font-bold">Mechatronics Engineer</strong>
-              </span>
-            </h1>
+            <div className="relative z-10">
+              <div className="flex animate-fade-left items-center gap-2 text-2xl text-slate-500 dark:text-slate-500 md:text-4xl">
+                Hi!
+              </div>
+              <h1 className="text-gray-600 dark:text-slate-400">
+                <span className="mb-3 block animate-fade-left text-5xl font-bold md:mb-4 md:text-7xl">
+                  I&apos;m{' '}
+                  <strong className="font-extrabold text-gray-900 dark:text-slate-50">
+                    Igmhar
+                  </strong>
+                  ,
+                </span>
+                <span className="block animate-fade-left text-xl animation-delay-100 md:text-2xl">
+                  <strong className="font-bold"> Jr. Software</strong>{' '}
+                  <strong className="font-bold">Developer</strong> and{' '}
+                  <strong className="font-bold">Mechatronics Engineer</strong>
+                </span>
+              </h1>
+            </div>
             {/* cta */}
             <div className="mt-8 flex gap-2">
               <Link
@@ -101,7 +80,7 @@ const Index: NextPage = () => {
                 <TechSlider />
               </div>
             </div>
-            <div className="absolute right-0 -top-28 hidden lg:block">
+            <div className="pointer-events-none absolute -top-16 right-0 z-0 hidden select-none lg:block">
               <Image
                 alt="me"
                 src="/me.jpeg"
