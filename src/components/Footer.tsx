c7750be5a928b774296d48bc2f3f82950c0ea4c5
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-10-07 12:24:59 -0600

    feat: Last commit on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,43 +1,36 @@
 import Link from 'next/link';
+import LastCommit from '@/components/LastCommit';
 
 const Footer = () => {
   return (
-    <footer className="border-t border-t-slate-100 py-8 px-4 text-sm text-gray-900">
-      <section className="mb-12 font-semibold">
-        <ul className="flex flex-wrap justify-center gap-x-4 gap-y-1">
-          <li>
-            <Link href="/">Mexico</Link>
-          </li>
-          <li className="w-full sm:hidden"></li>
-          <li>
-            <Link href="/">Source Code</Link>
-          </li>
-          <li>
-            <Link href="/">Algo</Link>
-          </li>
-          <li>
-            <Link href="/">Mail</Link>
-          </li>
-        </ul>
-      </section>
-      <section className="mb-2">
-        <ul className="flex justify-center gap-1 text-xs text-gray-500">
-          <li>
-            <Link href="/" className="hover:underline">
-              Holaa
-            </Link>
-          </li>
-          <li>·</li>
-          <li>
-            <Link href="/" className="hover:underline">
-              Pudiplay
-            </Link>
-          </li>
-        </ul>
-      </section>
-      <small className="flex justify-center text-sm font-semibold">
-        &copy; 2024, Igmhar Sánchez
-      </small>
+    <footer className="border-t border-slate-100 py-8 text-sm text-gray-900 dark:border-slate-800 dark:text-slate-200">
+      <div className="content-wrapper">
+        <section className="mb-12 font-semibold">
+          <ul className="flex flex-wrap justify-center gap-x-4 gap-y-1">
+            <li>
+              <Link href="/">Mexico</Link>
+            </li>
+            <li className="w-full sm:hidden"></li>
+            <li>
+              <Link href="/">Source Code</Link>
+            </li>
+            <li>
+              <Link href="/">Algo</Link>
+            </li>
+            <li>
+              <Link href="/">Mail</Link>
+            </li>
+          </ul>
+        </section>
+        <section className="mb-2">
+          <div className="flex justify-center text-xs text-gray-500 hover:underline dark:text-slate-400">
+            <LastCommit />
+          </div>
+        </section>
+        <small className="flex justify-center text-sm font-semibold">
+          &copy; 2024, Igmhar Sánchez
+        </small>
+      </div>
     </footer>
   );
 };
