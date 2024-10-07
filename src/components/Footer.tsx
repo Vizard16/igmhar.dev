56874a29331ff0c2de40943034a19e2cae54bd2c
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-10-07 16:21:34 -0600

    Refactor Footer component to add link to source code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import dayjs from '@/utils/dayjs';
 
 const Footer = () => {
   return (
-    <footer className="border-t border-slate-100 text-sm text-gray-900 dark:border-slate-800 dark:text-slate-200">
+    <footer className="border-t border-slate-100 bg-white text-sm text-gray-900 dark:border-slate-800 dark:bg-slate-900 dark:text-slate-200">
       <div className="content-wrapper">
         <section className="py-10 font-semibold">
           <ul className="flex flex-wrap justify-center gap-x-4 gap-y-1 lg:gap-x-6">
@@ -13,7 +13,13 @@ const Footer = () => {
             </li>
             <li className="w-full sm:hidden"></li>
             <li>
-              <Link href="/">Source Code</Link>
+              <a
+                href="https://github.com/Vizard16/igmhar.dev"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
             </li>
             <li>
               <Link href="/">Algo</Link>
